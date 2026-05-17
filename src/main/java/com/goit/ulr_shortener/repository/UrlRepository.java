c2fb0668768b4e130908d6a85dfe9312c2255afc
ideathreject <fury.extreme1@gmail.com>
2026-05-17 21:31:31 +0200

    change recursive to retry loop amd catch  DataIntegrityViolationException

--- src/main/java/com/goit/ulr_shortener/repository/UrlRepository.java
+++ src/main/java/com/goit/ulr_shortener/repository/UrlRepository.java
@@ -2,7 +2,11 @@ package com.goit.ulr_shortener.repository;
 
 import com.goit.ulr_shortener.entity.Url;
 import com.goit.ulr_shortener.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +17,8 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<Url> findByShortCode(String shortCode);
 
     List<Url> findAllByUser(User user);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Url u SET u.clickCount = u.clickCount + 1 WHERE u.shortCode = :shortCode")
+    void incrementClickCount(@Param("shortCode") String shortCode);
 }
