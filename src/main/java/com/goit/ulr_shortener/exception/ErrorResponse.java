794304ade7ed1c38ff3b29fdcedc4b34d98746c6
ideathreject <fury.extreme1@gmail.com>
2026-05-17 22:40:08 +0200

    add global exception @RestControllerAdvice

---
+++ src/main/java/com/goit/ulr_shortener/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.goit.ulr_shortener.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
