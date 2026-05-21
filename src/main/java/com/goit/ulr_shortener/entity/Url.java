b0846e50fe3d3c7a21892d72c8c662b156455f7d
ideathreject <fury.extreme1@gmail.com>
2026-05-21 18:50:25 +0200

    add not NULL to user_id

--- src/main/java/com/goit/ulr_shortener/entity/Url.java
+++ src/main/java/com/goit/ulr_shortener/entity/Url.java
@@ -32,6 +32,6 @@ public class Url {
     private LocalDateTime expiresAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
